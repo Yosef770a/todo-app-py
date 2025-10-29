fb3b0c72b0e534dd11b97dd9c17dd0ebd0b1109b
Yosef770a <moked7700@gmail.com>
2025-10-29 12:42:42 +0200

    Added the option to mark a task as completed

--- main.py
+++ main.py
@@ -38,6 +38,26 @@ def play():
                 print("Please enter a task number, the value must be a number")
 
         elif user_choice == 5:
+            keyword = input("\nPlease enter a new description: ")
+            list_found =todo.search_tasks(todo_list, keyword)
+            if len(list_found):
+                print(list_found)
+            else:
+                print("No matching word found in the task list")
+
+
+        elif user_choice == 6:
+            try:
+                number_to_complete = int(input("\nPlease enter a task number to completed: "))
+                if todo.mark_task_as_done(todo_list, number_to_complete - 1):
+                    print("The task has been marked as completed\n")
+                    todo.show_all_tasks(todo_list)
+                else:
+                    print("Error: The task number you entered does not exist. Please select a valid task number")
+            except ValueError:
+                print("Please enter a task number, the value must be a number")
+
+        elif user_choice == 7:
             print("Goodbye!")
             break
 
--- todo.py
+++ todo.py
@@ -11,7 +11,6 @@ def show_all_tasks(tasks: list) -> None:
     for i,v in enumerate(tasks):
         print(" ",i+1, v)
         
-# show_all_tasks(["yy", "hhh"])
 
 
 
@@ -26,10 +25,12 @@ Please select one of the following options:
     Show all tasks 2
     Delete a task 3
     Edit a task 4
-    Exit 5
+    Search for a word from the task list 5
+    Mark a task as completed 6                             
+    Exit 7
 
     """))
-            if 1 <= input_choice <= 5:
+            if 1 <= input_choice <= 7:
                 return input_choice
             else:
                 print()
@@ -37,7 +38,6 @@ Please select one of the following options:
         except ValueError:
             print("Please enter a numeric value only between 1 and 5")
 
-# get_user_choice()
 
 
 
@@ -49,22 +49,34 @@ def delete_task(tasks: list, index: int) -> bool:
         return False
 
 
-# tasks = ["yy", "hhh"]
-# print(delete_task(tasks, 3))
-
-
 
 def edit_task(tasks: list, index: int, new_task: str) -> bool:
     try:
-        print(tasks[index])
         tasks[index] = new_task
-        print(tasks[index])
         return True
     except IndexError:
         return False
-    
-# tasks = ["yy", "hhh"]
+
+
+def search_tasks(tasks: list, keyword: str) -> list:
+    task_word = []
+    for i,v in enumerate(tasks):
+        if keyword in v:
+            task_word.append((i, v))
+    return task_word
+
+
+
+def mark_task_as_done(tasks: list, index: int) -> bool:
+    try:
+        tasks[index] = tasks[index] + " ✓"
+        return True
+    except IndexError:
+        return False
+
+
+# tasks = ["yy", "hhh", "8gf7f"]
 # print(tasks)
 
-# print(edit_task(tasks, 1, "ee"))
+# print(edit_task(tasks, 0))
 # print(tasks)
