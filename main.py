54db9cd09454a2f578e5c071eae9720826c55fbb
Yosef770a <moked7700@gmail.com>
2025-10-29 11:36:25 +0200

    Added option to delete a task including protection against crashes due to incorrect input

--- main.py
+++ main.py
@@ -9,15 +9,26 @@ def play():
             new_task = input("\nPlease enter your task description: ")
             todo.add_task(todo_list, new_task)
             print("The task was added successfully!")
+
         elif user_choice == 2:
             if len(todo_list) == 0:
                 print("Your task list is empty, please add a new task.")
             else:
                 todo.show_all_tasks(todo_list)
+
         elif user_choice == 3:
-            print("This option will be added soon")
+            try:
+                number_to_delete = int(input("\nPlease enter a task number to delete: "))
+                if todo.delete_task(todo_list, number_to_delete - 1):
+                    print("The deletion was successful")
+                else:
+                    print("Error: The task number you entered does not exist. Please select a valid task number")
+            except ValueError:
+                print("Please enter a task number, the value must be a number")
+
         elif user_choice == 4:
             print("EThis option will be added soon")
+
         elif user_choice == 5:
             print("Goodbye!")
             break
--- todo.py
+++ todo.py
@@ -7,9 +7,9 @@ def add_task(tasks: list, task: str) -> None:
     
 
 def show_all_tasks(tasks: list) -> None:
-    print("The task list:")
+    print("\nThe task list:")
     for i,v in enumerate(tasks):
-        print(i+1, v)
+        print(" ",i+1, v)
         
 # show_all_tasks(["yy", "hhh"])
 
@@ -19,12 +19,14 @@ def get_user_choice() -> int:
     while True:
         try:
             input_choice = int(input(f"""
-Please select one of the following options:          
+                                     
+Please select one of the following options:   
+                                            
     Add a task 1
     Show all tasks 2
     Delete a task 3
-    Edit a task 4.
-    Exit .5
+    Edit a task 4
+    Exit 5
 
     """))
             if 1 <= input_choice <= 5:
@@ -36,3 +38,16 @@ Please select one of the following options:
             print("Please enter a numeric value only between 1 and 5")
 
 # get_user_choice()
+
+
+
+def delete_task(tasks: list, index: int) -> bool:
+    try:
+        tasks.pop(index)
+        return True
+    except IndexError:
+        return False
+
+
+# tasks = ["yy", "hhh"]
+# print(delete_task(tasks, 3))
